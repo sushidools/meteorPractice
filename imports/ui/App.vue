323c23d23a41e223a6cde3035379f68edbbaa0ec
sushidools <sashadooley@gmail.com>
2020-04-09 17:06:31 -0600

    filtering tasks

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -2,9 +2,26 @@
   <div class="container">
     <header>
       <h1>Todo List</h1>
+      <label className="hide-completed">
+        <input
+          type="checkbox"
+          readOnly
+          checked="hideCompleted"
+          v-model="hideCompleted"
+          @click="toggleHideCompleted"
+        />
+        Hide Completed Tasks
+      </label>
+      <form className="new-task" @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          placeholder="Type to add new tasks"
+          v-model="newTask"
+        />
+      </form>
     </header>
     <ul>
-      <Task v-for="task in getTasks()" v-bind:key="task._id" v-bind:task="task" />
+      <Task v-for="task in tasks" v-bind:key="task._id" v-bind:task="task" />
     </ul>
   </div>
 </template>
@@ -12,21 +29,35 @@
 <script>
 import Vue from "vue";
 import Task from "./Task.vue";
+import { Tasks } from "../api/tasks.js";
 
 export default {
   components: {
     Task
   },
   data() {
-    return {};
+    return {
+      newTask: "",
+      hideCompleted: false
+    };
   },
   methods: {
-    getTasks() {
-      return [
-        { _id: 1, text: "This is task 1" },
-        { _id: 2, text: "This is task 2" },
-        { _id: 3, text: "This is task 3" }
-      ];
+    handleSubmit(event) {
+      Tasks.insert({
+        text: this.newTask,
+        createdAt: new Date() //current time
+      });
+
+      // Clear form
+      this.newTask = "";
+    },
+    toggleHideCompleted() {
+      this.hideCompleted = !this.hideCompleted;
+    }
+  },
+  meteor: {
+    tasks() {
+      return Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
     }
   }
 };
