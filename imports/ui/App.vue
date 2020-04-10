7ce8ed1bf625d8c81da9df3d616af014d3823f47
sushidools <sashadooley@gmail.com>
2020-04-09 18:55:05 -0600

    adding user login

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <header>
       <h1>Todo List</h1>
+      <h4>You have {{ incompleteCount }} tasks left.</h4>
       <label className="hide-completed">
         <input
           type="checkbox"
@@ -12,13 +13,17 @@
         />
         Hide Completed Tasks
       </label>
-      <form className="new-task" @submit.prevent="handleSubmit">
-        <input
-          type="text"
-          placeholder="Type to add new tasks"
-          v-model="newTask"
-        />
-      </form>
+
+      <blaze-template template="loginButtons" tag="span"></blaze-template>
+      <template v-if="currentUser">
+        <form className="new-task" @submit.prevent="handleSubmit">
+          <input
+            type="text"
+            placeholder="Type to add new tasks"
+            v-model="newTask"
+          />
+        </form>
+      </template>
     </header>
     <ul>
       <Task v-for="task in tasks" v-bind:key="task._id" v-bind:task="task" />
@@ -45,7 +50,9 @@ export default {
     handleSubmit(event) {
       Tasks.insert({
         text: this.newTask,
-        createdAt: new Date() //current time
+        createdAt: new Date(), //current time
+        owner: Meteor.userId(), // _id of logged in user
+        username: Meteor.user().username // username of logged in user
       });
 
       // Clear form
@@ -57,7 +64,17 @@ export default {
   },
   meteor: {
     tasks() {
-      return Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
+      let filteredTasks = Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
+      if (this.hideCompleted) {
+        filteredTasks = filteredTasks.filter(task => !task.checked);
+      }
+      return filteredTasks;
+    },
+    incompleteCount() {
+      return Tasks.find({ checked: { $ne: true } }).count();
+    },
+    currentUser() {
+      return Meteor.user();
     }
   }
 };
